6ac02982e0757f0565078c270b1213081cddb60a
Wesley Neves <wesleyneves675@gmail.com>
2026-08-04 14:48:26 -0400

    classes Pessoa e classe Main criadas

---
+++ src/Pessoa.java
@@ -0,0 +1,50 @@
+
+public class Pessoa {
+	private String nome;
+	private int idade;
+	private double altura;
+	
+	
+	public Pessoa(String nome, int idade, double altura) {
+		this.nome = nome;
+		this.idade = idade;
+		this.altura = altura;
+	}
+	
+	public Pessoa() {
+		
+	}
+	
+	public void exibirDados() {
+		System.out.println("Nome: " + this.nome);
+		System.out.println("Idade: " + this.idade);
+		System.out.println("Altura: " + this.altura);
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public double getAltura() {
+		return altura;
+	}
+
+	public void setAltura(double altura) {
+		this.altura = altura;
+	}
+	
+	
+	
+}
