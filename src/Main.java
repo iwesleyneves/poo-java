6806bdb954b76167fc89bd28dd9163856c8de933
Wesley Neves <wesleyneves675@gmail.com>
2026-08-04 15:18:42 -0400

    classe Calculadora criada

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,29 @@
 public class Main {
 	public static void main(String[] args) {
 		
-		Pessoa pessoa1 = new Pessoa("Wesley", 23, 1.70);
+//		Pessoa pessoa1 = new Pessoa("Wesley", 23, 1.70);
+//		
+//		Pessoa pessoa2 = new Pessoa();
+//		
+//		pessoa2.setNome("Marley");
+//		pessoa2.setIdade(20);
+//		pessoa2.setAltura(1.74);
+//		
+//		pessoa1.exibirDados();
+//		
+//		pessoa2.exibirDados();
 		
-		Pessoa pessoa2 = new Pessoa();
+		double resultadoSoma = Calculadora.somar(10, 5);
+		System.out.println("Soma: "+resultadoSoma);
 		
-		pessoa2.setNome("Marley");
-		pessoa2.setIdade(20);
-		pessoa2.setAltura(1.74);
+		double resultadoSubtracao = Calculadora.subtrair(10, 5);
+		System.out.println("Subtração: "+resultadoSubtracao);
 		
-		pessoa1.exibirDados();
+		double resultadoMultiplicacao = Calculadora.multiplicar(10, 5);
+		System.out.println("Multiplicação: "+resultadoMultiplicacao);
 		
-		pessoa2.exibirDados();
+		double resultadoDivisao = Calculadora.dividir(10, 5);
+		System.out.println("Divisão: "+resultadoDivisao);
 	}
+	
 }
